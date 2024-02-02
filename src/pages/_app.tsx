0fb83959d5cfcdcdbeda23dfe02469dd5f13b9b9
pHMiguel <aphonso99@gmail.com>
2024-02-02 09:44:11 -0300

    production_branch_init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Navbar, BackgroundSelector, BackgroundComponent } from "@/components";
+import { Navbar } from "@/components";
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
 import { Raleway } from "next/font/google";
 import Head from "next/head";
+import LazyLoad from "react-lazy-load";
 
 const raleway = Raleway({
   style: ["normal", "italic"],
@@ -12,30 +13,31 @@ const raleway = Raleway({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [selectedBackground, setSelectedBackground] = useState("1");
-
-  const handleBackgroundChange = (selectedBackground: any) => {
-    setSelectedBackground(selectedBackground);
-  };
-
   return (
     <div className={raleway.className}>
       <Head>
         <title>{"Aphonso Portfolio"}</title>
       </Head>
       <main>
+        <LazyLoad offset={50}>
+          <video
+            src="../assets/videos/bg_video_01.mp4"
+            autoPlay
+            loop
+            muted
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              width: "100vw",
+              height: "100vh",
+              objectFit: "cover",
+              zIndex: -1,
+            }}
+          />
+        </LazyLoad>
         <Navbar />
         <Component {...pageProps} />
-        {selectedBackground === "1" && (
-          <BackgroundComponent videoSrc="../assets/videos/bg_video_01.mp4" />
-        )}
-        {selectedBackground === "2" && (
-          <BackgroundComponent videoSrc="../assets/videos/bg_video_02.mp4" />
-        )}
-        {selectedBackground === "3" && (
-          <BackgroundComponent videoSrc="../assets/videos/bg_video_03.mp4" />
-        )}
-        <BackgroundSelector onBackgroundChange={handleBackgroundChange} />
       </main>
     </div>
   );
